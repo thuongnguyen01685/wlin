4b13082d4e406437bce587bfa779915cafd35146
@thuongnguyen01685 <thuong.nguyen@fostech.vn>
2022-10-28 10:03:10 +0700

    share socket

---
+++ screens/events/TabEvent/EventingRoute.js
@@ -0,0 +1,222 @@
+//import liraries
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import React, { Component, useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  RefreshControl,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+} from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getDetailEventsAction,
+  getEventsAction,
+} from "../../../redux/actions/eventsAction";
+import { formatDateDisplay, formatDateDisplays } from "../../../utils/datetime";
+import { URL } from "../../../utils/fetchApi";
+
+const w = Dimensions.get("window").width;
+const h = Dimensions.get("window").height;
+const wait = (timeout) => {
+  return new Promise((resolve) => setTimeout(resolve, timeout));
+};
+
+// create a component
+const EventingRoute = () => {
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const [refreshing, setRefreshing] = useState(false);
+  const { auth, event } = useSelector((state) => state);
+
+  const handleDetail = (_id) => {
+    dispatch(getDetailEventsAction(_id, auth.token));
+    navigation.navigate("DetailEvents");
+  };
+
+  let dateNow = new Date();
+  let year = dateNow.getFullYear();
+  let month = dateNow.getMonth() + 1;
+  let day = dateNow.getDate();
+  let dayofweek = dateNow.getDay();
+
+  const dayNow = year + "-" + month + "-" + day;
+
+  const dayname = ["CN", "T2", "T3", "T4", "T5", "T6", "T7"];
+
+  const eventing = event.getEvents.filter(
+    (item) =>
+      new Date(formatDateDisplays(item.ngay_su_kien)).getTime() >
+      new Date(dayNow).getTime()
+  );
+
+  useEffect(() => {
+    setRefreshing(true);
+    dispatch(getEventsAction(auth.token));
+    wait(2000).then(() => setRefreshing(false));
+  }, [dispatch]);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    dispatch(getEventsAction(auth.token));
+    wait(2000).then(() => setRefreshing(false));
+  }, [dispatch]);
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            colors={["#9796F0", "green", "blue"]}
+          />
+        }>
+        <View
+          style={{
+            marginTop: 10,
+            height: "100%",
+            marginBottom: "75%",
+          }}>
+          {eventing.length > 0 ? (
+            eventing.map((item) => (
+              <TouchableOpacity
+                key={item._id}
+                style={{
+                  flexDirection: "row",
+                  marginBottom: 10,
+                  borderRadius: 8,
+                  paddingVertical: 5,
+                  marginHorizontal: 15,
+                  borderBottomColor: "#DADADA",
+                  borderBottomWidth: 0.5,
+                }}
+                onPress={() => handleDetail(item._id)}>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    width: "80%",
+                    marginBottom: 5,
+                  }}>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      borderRadius: 7,
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}>
+                    <Image
+                      source={{ uri: `${URL}${item.hinh_anh}` }}
+                      style={{ width: 80, height: 80, borderRadius: 7 }}
+                    />
+                  </View>
+                  <View
+                    style={{
+                      width: "75%",
+                      justifyContent: "space-evenly",
+                      alignItems: "stretch",
+                      marginLeft: 10,
+                    }}>
+                    <Text
+                      style={{
+                        color: "#474747",
+                        fontSize: 14,
+                        fontWeight: "600",
+                      }}>
+                      {item.ten_su_kien}
+                    </Text>
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                        backgroundColor: "#EEF4FF",
+                        width: "50%",
+                        borderRadius: 13,
+                        paddingHorizontal: 4,
+                        paddingVertical: 2,
+                      }}>
+                      <Ionicons name="calendar" size={15} color="#769CEC" />
+                      <Text
+                        style={{
+                          color: "#769CEC",
+                          fontSize: 11,
+                          fontWeight: "600",
+                          left: 10,
+                        }}>
+                        {formatDateDisplay(item.ngay_su_kien)}
+                      </Text>
+                    </View>
+                    <View style={{ flexDirection: "row" }}>
+                      <Ionicons name="location" size={14} />
+                      <Text
+                        style={{
+                          color: "#000000",
+                          fontSize: 10,
+                          fontWeight: "600",
+                          left: 5,
+                        }}>
+                        {item.dia_diem}
+                      </Text>
+                    </View>
+                  </View>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+
+                    width: "15%",
+                    marginLeft: 10,
+                  }}>
+                  <TouchableOpacity>
+                    <Ionicons
+                      name={item.save ? "bookmark" : "bookmark-outline"}
+                      size={20}
+                      color={item.save ? "#FFBE17" : "#FEC90F"}
+                    />
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <Ionicons
+                      name="alert-circle-outline"
+                      size={20}
+                      color="#9D85F2"
+                    />
+                  </TouchableOpacity>
+                </View>
+              </TouchableOpacity>
+            ))
+          ) : (
+            <View
+              style={{
+                paddingVertical: 15,
+                height: 200,
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}>
+              <Text
+                style={{ fontSize: 15, color: "#474747", fontWeight: "600" }}>
+                Không có sự kiện nào sắp diễn ra.
+              </Text>
+            </View>
+          )}
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+//make this component available to the app
+export default EventingRoute;
---
+++ utils/pushNotify.js
@@ -0,0 +1,241 @@
+import React, { Component, useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  DeviceEventEmitter,
+  Platform,
+} from "react-native";
+import * as Notifications from "expo-notifications";
+import io from "socket.io-client";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+export const initSocket = async (auth) => {
+  const server_url = "https://api.wlin.com.vn";
+  //get func
+  let lang,
+    cachedData = {};
+  function get(url, headers, callback, options = { cache: false }) {
+    if (cachedData[url] && options.cache) {
+      //console.log("get from cached",url);
+      return callback(null, cachedData[url]);
+    }
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = (e) => {
+      if (request.readyState !== 4) {
+        return;
+      }
+      if (request.status === 200) {
+        let data = request.responseText;
+        if (options.onProccess) {
+          data = options.onProccess(data);
+        }
+        if (options.cache) cachedData[url] = data;
+        callback(null, request.responseText);
+      } else {
+        let error = request.responseText || "";
+        if (!error || error.indexOf("flexbiz") >= 0)
+          error = "Can't connect to server";
+        callback(error);
+      }
+    };
+    request.open("GET", url);
+    request.setRequestHeader("Accept-Encoding", "gzip");
+    if (headers) {
+      headers.forEach((header) => {
+        request.setRequestHeader(header.name, header.value);
+      });
+    }
+    request.send();
+  }
+
+  //asyncGet func
+  async function asyncGet(url, headers, options = {}) {
+    let p = new Promise((resovle, reject) => {
+      get(
+        url,
+        headers,
+        (e, rs) => {
+          if (e) return reject(new Error(e));
+          resovle(rs);
+        },
+        options
+      );
+    });
+    return p;
+  }
+
+  // registerForPushNotificationsAsync func
+
+  async function registerForPushNotificationsAsync(access_token) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+
+    if (finalStatus !== "granted") {
+      //Alert.alert(getLabel("Chương trình này cần được cấp quyền hiển thị thông báo"))
+      return false;
+    }
+    // Get the token that uniquely identifies this device
+    let token = (await Notifications.getExpoPushTokenAsync()).data;
+    // POST the token to your backend server from where you can retrieve it to send push notifications.
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("chat-messages", {
+        name: "chat-messages",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+    try {
+      let rs = await asyncGet(
+        `${server_url}/api/register-endpoint?access_token=${access_token}&ep=${token}`
+      );
+      await AsyncStorage.setItem("endpoint", token);
+    } catch (e) {
+      return false;
+    }
+    return true;
+  }
+
+  let notificationPermission;
+  try {
+    notificationPermission = await registerForPushNotificationsAsync(
+      // JSON.parse(value).token,
+      auth.token
+    );
+
+    //receiver notification
+    if (notificationPermission == false) {
+      Notifications.addNotificationReceivedListener((res) =>
+        _handleNotification(res)
+      );
+
+      Notifications.addNotificationResponseReceivedListener((res) =>
+        _handleNotificationResponse(res)
+      );
+    }
+  } catch (e) {
+    console.log(e.message);
+  }
+
+  //connect socket
+
+  const socket = io(server_url);
+  socket.on("connect", () => {
+    socket.emit("login", {
+      // token: "2b0350de2b7100a6562ecb87b40ae520",
+      // email: "thuong.nguyen@fostech.vn",
+      token: auth.token,
+      email: auth.profile && auth.profile.email,
+    });
+  });
+
+  socket.on("disconnect", function () {
+    // console.error("socket disconnectedf");
+  });
+
+  socket.on("reconnect_error", function () {
+    console.error("socket reconnect error");
+  });
+  socket.on("connect_error", function () {
+    console.error("socket connect error");
+  });
+  socket.on("connect_timeout", (timeout) => {
+    console.error("socket connect timeout", timeout);
+  });
+
+  socket.emit("login", {
+    token: auth.token,
+    email: auth.profile && auth.profile.email,
+    // token: "2b0350de2b7100a6562ecb87b40ae520",
+    // email: "thuong.nguyen@fostech.vn",
+  });
+
+  socket.on("notify:update", function (data) {
+    DeviceEventEmitter.emit("notificationUpdated", data);
+  });
+
+  function _handleNotification(notification) {
+    let data = notification.request.content.data;
+    DeviceEventEmitter.emit(data.__event, data);
+  }
+  async function _handleNotificationResponse(response) {
+    let data = response.notification.request.content.data;
+    //news
+    if (data.__event && data.__event.indexOf("news:") >= 0) {
+      if (data._id) {
+        try {
+          let news = await asyncGetOrderDetail(
+            this.state.userInfo.token,
+            data._id,
+            "news",
+            data.id_app
+          );
+          this.navigate("NewsDetail", { news: news });
+        } catch (e) {
+          console.error(e.message);
+        }
+      }
+      return;
+    }
+
+    // if (data.__event && data.__event.indexOf("message2:") >= 0) {
+    //   if (data.id_link) {
+    //     try {
+    //       let group = await asyncGetData(
+    //         this.state.userInfo.token,
+    //         "message2",
+    //         { id_link: data.id_link }
+    //       );
+    //       this.navigate("ChatRoom", { group: group });
+    //     } catch (e) {
+    //       console.error(e.message);
+    //     }
+    //     return;
+    //   }
+    // }
+
+    if (data.__event && data.__event.indexOf("notify:new") >= 0 && data._id) {
+      DeviceEventEmitter.emit("notificationAdded", { _id: data._id });
+      try {
+        let url = `${server_url}/api/notification/${data._id}?access_token=${this.state.userInfo.token}`;
+        let n = await asyncGet(url, null);
+        this.navigate("NotificationDetail", {
+          userInfo: this.state.userInfo,
+          notification: n,
+        });
+      } catch (e) {
+        console.error(e.message);
+      }
+      return;
+    }
+    Toast.show({
+      text: data.title || getLabel("Bạn có thông báo mới"),
+      buttonText: "Okay",
+      duration: 3000,
+      type: "danger",
+      position: "top",
+    });
+  }
+};
