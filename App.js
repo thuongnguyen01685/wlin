4cd80457c09c48fc4e6d22fcfec098bd0f9d79d6
@thuongnguyen01685 <thuong.nguyen@fostech.vn>
2022-11-24 11:48:07 +0700

    fix img

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
 import { Provider } from "react-redux";
 import Navigator from "./navigation/Navigator";
 import { store } from "./redux/store";
+import * as Updates from "expo-updates";
+import { Alert } from "react-native";
 
 export default function App() {
+  Updates.checkForUpdateAsync().then((update) => {
+    if (update.isAvailable) {
+      Updates.fetchUpdateAsync().then((rs) => {
+        if (rs.isNew) {
+          Alert.alert(
+            " UPDATE",
+            "Chương trình có cập nhật mới. Bạn hãy khởi động lại chương trình để áp dụng phiên bản mới nhất!",
+            [
+              {
+                text: "restart",
+                onPress: () => {
+                  return Updates.reloadAsync();
+                },
+              },
+            ]
+          );
+        }
+      });
+    }
+  });
   return (
     <Provider store={store}>
       <Navigator />
